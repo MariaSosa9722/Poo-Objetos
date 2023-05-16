f2cab424b3d0e820935047caa461f19834ac106f
MariaSosa9722 <Mariasosa9722@gmail.com>
2023-05-16 11:17:43 -0600

    subiendo el repo

---
+++ Clases/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    //clases es una plantilla 
+    //Que contiene Objetos y Metodos
+    public class Persona
+    {
+       public string Nombre { get; set; }
+       public string Apellido { get; set; }
+       public int Edad { get; set; }
+       public double Salario { get; set; }  
+
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Tu nombre es :"+this.Nombre);
+            Console.WriteLine("Tu Apellido es :" + this.Apellido);
+            Console.WriteLine("Tu edad es :" + this.Edad);
+            Console.WriteLine("El Salario es :" + this.Salario);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona persona = new Persona();
+
+            persona.Nombre = "Maria";
+            persona.Apellido = "Sosa";
+            persona.Edad = 25;
+            persona.Salario = 12313.313;
+
+            persona.ImprimirDatos();
+
+            Console.WriteLine("--------------------");
+            Persona persona2 = new Persona();
+
+            persona2.Nombre = "Jose";
+            persona2.Apellido = "Yam";
+            persona2.Edad = 35;
+            persona2.Salario = 12313.313;
+            
+            persona2.ImprimirDatos();
+
+        }
+    }
+}
